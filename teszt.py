1d1e57fc8653d8c5aaae315a5997e5958cd0e371
GumballPlat4 <162676451+GumballPlat4@users.noreply.github.com>
2024-03-22 09:02:17 +0100

    Create teszt.py

---
+++ teszt.py
@@ -0,0 +1,17 @@
+
+sum(lista)
+
+def osszegzes(l):
+    sum=0
+    
+    for szam in l:
+        sum+=szam
+    return szam
+    
+
+def kiiras():
+    print(veg_osszeg)
+
+veg_osszeg=osszegzes
+
+kiiras(veg_osszeg)
